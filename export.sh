99aa066b6b72f96a1a75ad15e086ca21c7378a4f
Timothy Day <tday141@gmail.com>
2022-11-23 23:09:51 -0500

    Modify PATH in export.sh

--- export.sh
+++ export.sh
@@ -2,10 +2,13 @@
 # Script to point towards all other scripts
 
 
-export S_PATH="$(dirname "$(realpath ${BASH_SOURCE})")"
+export S_PATH="$(dirname "$(realpath ${BASH_SOURCE})")/src"
 export S_LIB_PATH="$(dirname "$(realpath ${BASH_SOURCE})")/src/lib"
 source "${S_LIB_PATH}/script-helper.sh"
 source_me
+
+
+export PATH="${S_PATH}:$PATH"
 
 
 if [ "$1" == "-v" ]; then
